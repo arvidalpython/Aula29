63709b4ab020524b1dbaa60ac072341946bcba56
unknown <arvida.python@gmail.com>
2024-10-13 16:25:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import Teste from './Teste';
+
+
+
+function App() {
+  return <Teste/>
+}
+
+export default App;
---
+++ src/Teste.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste(){
+  return <div>Olá</div>
+}
+export default Teste
